fa3d7e4ee56beb1dac991ab2155723df68b1b653
NaiSaratxaga <nsaratxaga@gmail.com>
2025-03-12 13:01:24 +0100

    inicio hlf

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # Vamos llamando a las funciones
+# con control c puedes salir del programa
+
 
 from funciones import*
+from variables import *
 
 import pprint
 import funciones
+import time
+import os
 
+
+mensaje_bienvenida()
 tablero_computer = crear_tablero(10)
 tablero_computer_visualizar = crear_tablero(10)
+print()
+
+
+    
+    
---
+++ variables.py
@@ -0,0 +1,3 @@
+# para que el usuario puede tocar la configuración antes de jugar
+
+tamano = 10

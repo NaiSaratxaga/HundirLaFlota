637282c890c7bebdb882f187480b9c86fcb39a75
NaiSaratxaga <nsaratxaga@gmail.com>
2025-03-12 13:26:05 +0100

    Refactor game logic: modularize board creation and shooting functions

--- funciones.py
+++ funciones.py
@@ -1,49 +1,55 @@
-tablero = [[" " for i in range(10)] for j in range(10)]
 
-tablero
-
-tablero
-tablero[3][3] = 'B'
-tablero[4][3] = 'B'
-tablero[5][3] = 'B'
-tablero[6][3] = 'B'
-tablero
+import pprint
+from clases import *
 
 
-tablero[1][0] = 'B'
-tablero[1][1] = 'B'
-tablero[1][2] = 'B'
-tablero[1][3] = 'B'
-tablero
+def crear_tablero(tamano):
+    tablero = [[" " for i in range(tamano)] for j in range(tamano)]
+    return tablero
 
 
-def disparo(tablero,i,j):
+def posicionar_barcos_fijos(tablero):
+    tablero[3][3] = 'B'
+    tablero[4][3] = 'B'
+    tablero[5][3] = 'B'
+    tablero[6][3] = 'B'
+
+    tablero[1][0] = 'B'
+    tablero[1][1] = 'B'
+    tablero[1][2] = 'B'
+    tablero[1][3] = 'B'
+    
+    
+    
+def disparo(tablero,tablero_mostrar,i,j):
     if tablero[i][j] == 'B':
         print("Tocado")
         tablero[i][j] = "X"
+        tablero_mostrar[i][j] = "X"
         return True
     elif tablero[i][j] == " ":
         print("Agua")
         tablero[i][j] = "O"
+        tablero_mostrar[i][j] = "X"
         return False
     else:
         print("Ya habías disparado allí, inútil")
         return False
-        
-
-
-disparo(tablero,0,0)
-tablero
-
-
-disparo(tablero,1,0)
-tablero
-
-disparo(tablero,1,0)
-tablero
-
-while True:
-    i = int(input("Introduce la fila, por favor"))
-    j = int(input("Introduce la columna, por favor"))
-    if not disparo(tablero,i,j):
-        break
+    
+    
+    
+def visualizar(tablero):
+    pprint.pprint(tablero)
+    
+    
+    
+def mensaje_bienvenida():
+    print("Bienvenido al juego de HLF")
+    print()
+    
+    
+    
+    
+    
+    
+    
